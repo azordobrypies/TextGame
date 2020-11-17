964eeeb12e553fd0e3b95195b2d1cd1da229b462
Michał Otrząsek <michuuuzzz1@gmail.com>
2020-11-17 09:35:05 +0100

    Added script that says "Hello World" in the console

---
+++ TextGame/Assets/Scripts/TextGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello world");
+    }
+
+    void Update()
+    {
+        
+    }
+}
